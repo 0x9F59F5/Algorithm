d3924148740760ac20812df79d5856affededf1d
Lee Min Woo <111226635+0x9F59F5@users.noreply.github.com>
2024-03-25 10:41:45 +0900

    Create CodeUp_1529.java

---
+++ CodeUp/CodeUp_1529.java
@@ -0,0 +1,17 @@
+package CodeUp;
+
+public class CodeUp_1529 {
+    
+    public static void f() {
+        
+        for (int i = 0; i < 2; i++) {
+            System.out.print("*");
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        Main.f();
+    }
+}
